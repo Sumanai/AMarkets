54bf80f403ad66fdfdcf2676af25314ccf37c5b7
Sumanai <sumanai@ya.ru>
2022-03-03 21:11:57 +0400

    Переименовал контроллер

--- routes/api.php
+++ routes/api.php
@@ -1,20 +1,20 @@
 <?php
 
-use App\Http\Controllers\Poster;
+use App\Http\Controllers\PosterController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 // Для простоты можно было бы так, но по ТЗ надо делать защиту на nginx, а вешать ограничения на HTTP методы сложнее
-// Route::resource('poster', Poster::class);
+// Route::resource('poster', PosterController::class);
 
 Route::prefix('poster')->group(function () {
-    Route::get('/', [Poster::class, 'index'])
+    Route::get('/', [PosterController::class, 'index'])
         ->name('posterIndex');
 
-    Route::get('/create', [Poster::class, 'create'])
+    Route::get('/create', [PosterController::class, 'create'])
         ->name('posterCreate');
 
-    Route::get('/delete/{id}', [Poster::class, 'delete'])
+    Route::get('/delete/{id}', [PosterController::class, 'delete'])
         ->where('id', '[0-9]+')
         ->name('posterDelete');
 });
